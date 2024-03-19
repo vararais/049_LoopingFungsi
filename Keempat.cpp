163336765899512586595d6899ecfeeedce3b752
vararais <varadigmaisnann@gmail.com>
2024-03-19 20:30:01 +0700

    Menambah Int hitungharga, Void display, dan Int Main

--- Keempat.cpp
+++ Keempat.cpp
@@ -16,3 +16,31 @@ void input()
     cout << "Masukan Jumlah Air Mineral = ";
     cin >> nAir;
 }
+
+int hitungharga()
+{
+    return (nTelor * hTelor) + (nMie * hMie) + (nAir * hAir);
+}
+
+void display()
+{
+    cout << "Nama = " << nama << endl;
+    cout << "Jumlah Telor = " << nTelor << endl;
+    cout << "Jumlah Mie = " << nMie << endl;
+    cout << "Jumlah Air = " << nAir << endl;
+    cout << "Total Harga Rp." << hitungharga() << endl;
+}
+
+int main()
+{
+    char pilihan;
+    do
+    {
+        input();
+        display();
+
+        cout << "Apakah anda ingin mengulangi (y/n) ? ";
+        cin >> pilihan;
+
+    } while (pilihan == 'y' || pilihan == 'Y');
+}
