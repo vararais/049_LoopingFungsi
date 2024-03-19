5eaa9754670fc067e6075689ac9b2b7c8e2acc02
vararais <varadigmaisnann@gmail.com>
2024-03-19 20:18:13 +0700

    Menambah Int Main

--- Pertama.cpp
+++ Pertama.cpp
@@ -1,3 +1,14 @@
 #include<iostream>
 using namespace std;
 
+int main(){
+
+    int i;
+string nama[5];
+
+for (i = 150; i > 100; i-+10)
+{
+    cout << i << ". "<< "SELAMAT PUASA" << endl;
+
+}
+cout << "Nilai i Terakhir = " << i << endl;
