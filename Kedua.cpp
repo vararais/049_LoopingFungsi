8a76e44c1ad53b586f8dd681057f8b41ee05eb49
vararais <varadigmaisnann@gmail.com>
2024-03-19 20:23:19 +0700

    Menambah Int Main

--- Kedua.cpp
+++ Kedua.cpp
@@ -1,3 +1,21 @@
 #include<iostream>
 using namespace std;
 
+
+int main(){
+    int bilangan;
+
+    srand(time(0));
+
+    bilangan = rand() % 10;
+
+    while (bilangan > 4) 
+    {
+        cout << "Bilangan lebih dari 4" << endl;
+        bilangan = rand() % 10;
+        cout << "Bilangannya = " << bilangan << endl;
+
+    }
+    cout << "Bilangan Terakhir = " << bilangan << endl;
+
+}
