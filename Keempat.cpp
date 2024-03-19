7c8781e3cff6449746b56fa0a273afc1889a5a73
vararais <varadigmaisnann@gmail.com>
2024-03-19 20:27:04 +0700

    Menambah Void Input

--- Keempat.cpp
+++ Keempat.cpp
@@ -5,3 +5,14 @@ int nTelor, nMie, nAir;
 int hTelor = 2000, hMie = 2800, hAir = 3000;
 string nama;
 
+void input()
+{
+    cout << "Masukan Nama = ";
+    cin >> nama;
+    cout << "Masukan Jumlah Telor = ";
+    cin >> nTelor;
+    cout << "Masukan Jumlah Mie = ";
+    cin >> nMie;
+    cout << "Masukan Jumlah Air Mineral = ";
+    cin >> nAir;
+}
